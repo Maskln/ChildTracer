c7c2d431664739086bf3b764e6829ebd04969047
Maskln <lyubomir.nanev@gmail.com>
2016-02-01 23:22:18 +0200

    Added empty project

---
+++ ChildTracer/ChildTracer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChildTracer
+//
+//  Created by Mask on 2/1/16.
+//  Copyright © 2016 Mask. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChildTracer/ChildTracer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChildTracer
+//
+//  Created by Mask on 2/1/16.
+//  Copyright © 2016 Mask. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

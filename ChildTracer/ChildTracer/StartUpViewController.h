2c49e3ede9bb98ef4e80faba44d9e600c5956915
Maskln <lyubomir.nanev@gmail.com>
2016-02-02 21:24:07 +0200

    Added StartViewController

---
+++ ChildTracer/ChildTracer/StartUpViewController.h
@@ -0,0 +1,15 @@
+//
+//  StartUpViewController.h
+//  ChildTracer
+//
+//  Created by Mask on 2/1/16.
+//  Copyright © 2016 Mask. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StartUpViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+
+@end
